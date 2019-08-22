1cb88d4990b070f6cdfd611808450c891d79e026
Yuseon <mongku90@gmail.com>
2019-08-22 21:27:54 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/')
+def index():
+    return 'this is my homepage!'
+
+@app.route('/yuseon')
+def yuseon():
+    return render_template('introduce.html')
+
+if __name__ == '__main__':
+    app.run()
