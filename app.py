ac273467ee897a4f43ccfb958793851165fb8de9
Yuseon <mongku90@gmail.com>
2019-09-22 15:06:16 +0900

    first commit

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route('/')
-def index():
-    return 'this is my homepage!'
+line_bot_api = LineBotApi('Your Channel Access Token')
+handler = WebhookHandler('36d86887eff5c9fd4acddc59940c1650')
 
-@app.route('/yuseon')
-def yuseon():
-    return render_template('intro.html')
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == '__main__':
     app.run()
